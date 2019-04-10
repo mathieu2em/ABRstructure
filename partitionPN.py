4ea9ec80e59316fac9d453b78aa7526dedf0f95c
mathieu <math.per@hotmail.com>
2019-04-10 12:39:32 -0400

    improved comments

--- partitionPN.py
+++ partitionPN.py
@@ -1,5 +1,5 @@
+#suce moer
 import math
-
 
 class BinaryTree:
     def __init__(self, rootkey, rootvalue=0):
@@ -102,16 +102,13 @@ class BinaryTree:
             self.__printBST__(node.left)
             self.__printBST__(node.right)
 
-
 class PartitionPN:
 
     # We will use a BST to represent our partition of positive numbers from 0(included) to m(excluded)
     # each node of the BST will represent a split in the partition.
     # for example if we have a partition composed of one continuous
     # suite of positive numbers from
-    # [0 , 100[ will have two nodes : the root will be 100 and the left node will be 0.
-    # to know the size of an interval , the left number ( here zero ) is represented by the node
-    # to the left or our own node if the node to the left is null . and the right number is the actual node.
+    # [0 , 100[  it will have 1 node : the root will be 100 .
     def __init__(self, m):
 
         self.BT = BinaryTree(m,m)
@@ -125,9 +122,9 @@ class PartitionPN:
         return self.biggest
 
     # return interval [Xi-1, Xi[ containing x
-    # while searching it will always keep in mind the latest bigger than x visited
+    # while searching it will always keep in mind the smallest bigger than x visited
     # so that when arriving in front of a smaller while having a bigger in memory it will
-    # return the partition from the smaller to the one in memory
+    # take the smaller bigger then x as the msb
     # METHODE RECHERCHER
     def rechercher(self, x):
         return self.__rechercher__(self.BT.root, x)
@@ -149,8 +146,10 @@ class PartitionPN:
             print("error")
             return
 
-    # this method will search for a certain interval and delete the node between this interval and the one bigger
-    # than him. it will use the same pattern as search and then delete the correct node using the delete method from
+    # this method will search for a certain interval and delete the node between
+    # this interval and the one bigger
+    # than him. it will use the same pattern as search and then delete the correct
+    # node using the delete method from
     # BST class
     def fusion(self, x):
         if x == self.biggest:
@@ -158,15 +157,19 @@ class PartitionPN:
         self.BT.delete(self.rechercher(x)[0])
         return
 
-    # the tranche function will split the partition into two smaller ones the one to the left excluding x and the one
-    # to right including it. The way it will do so is simply like a normal put(x) . it will search to the right place
-    # in the tree and place the node there. It is important to keep in mind that the binary search tree serves as a
-    # representation of the splitting positions in our partition. and that the smallest element and biggest wich are
-    # set up at the creation of the partition are kept separatey in "biggest" and "smallest" . by doing so we insure
-    # that the tree will be more balanced
+    # the tranche function will split the partition into two smaller ones
+    # the one to the left excluding x and the one
+    # to right including it. The way it will do so is simply like a normal put(x) .
+    # it will search to the right place
+    # in the tree and place the node there. It is important to keep in mind that
+    # the binary search tree serves as a
+    # representation of the splitting positions in our partition. and that
+    # the smallest element and biggest which are
+    # set up at the creation of the partition are kept separately in "biggest" and "smallest" .
+    # by doing so we insure
+    # that the tree will be more balanced and will always works
     def tranche(self, x):
         self.BT.put(x, x)
-
 
 class MainTest:
 
